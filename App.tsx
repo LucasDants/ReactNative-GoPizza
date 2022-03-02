d7cba15d2257920c46edbb44314bdc79f5e5e888
Lucas Dantas <lucasdantasmoura01@gmail.com>
2022-03-01 21:38:45 -0300

    Tela de SignIn

--- App.tsx
+++ App.tsx
@@ -4,7 +4,9 @@ import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans'
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
 import AppLoading from 'expo-app-loading'
 import { ThemeProvider} from 'styled-components/native'
-import theme from '@src/theme';
+import theme from './src/theme';
+import { SignIn } from '@screens/SignIn';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,6 +20,8 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <SignIn />
     </ThemeProvider>
   );
 }
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,42 @@
+import { Button } from '@components/Button';
+import { Input } from '@components/Input';
+import React from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+import brandImg from '@assets/brand.png'
+
+import {
+  Brand,
+  Container, Content, ForgotPasswordButton, ForgotPasswordLabel, Title
+} from './styles';
+
+export function SignIn(){
+  return (
+    <Container>
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+          <Content>
+            <Brand source={brandImg} />
+            <Title>Login</Title>
+            <Input 
+                placeholder='E-mail'
+                type='secondary'
+                autoCorrect={false}
+                autoCapitalize="none"
+            />
+            <Input 
+                placeholder='Senha'
+                type='secondary'
+                secureTextEntry
+            />
+            <ForgotPasswordButton>
+              <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+            </ForgotPasswordButton>
+
+            <Button 
+              title="Entrar"
+              type='secondary'
+            />
+          </Content>
+      </KeyboardAvoidingView>
+    </Container>
+  );
+}
